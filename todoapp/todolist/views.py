2ab99614f7f3056a5e7f4c0b2980d05a4b6260dd
unknown-bytes <unknown.develops@gmail.com>
2023-07-06 16:31:57 -0400

    post function added

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
+from .models import Todo
 
 # Create your views here.
 def index(request):
+    todos = Todo.objects.all()
     return render(request, "base.html", {"todo_list": todos})
+# post function
+@require_http_methods(["POST"])
+def add(request):
+    title = request.POST["title"]
+    todo = Todo(title=title)
+    todo.save()
+    return redirect("index")
