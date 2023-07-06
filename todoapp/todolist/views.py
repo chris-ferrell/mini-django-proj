0535e1e04dd8feb5d32a851dce7b8dd1766e377f
unknown-bytes <unknown.develops@gmail.com>
2023-07-06 16:36:13 -0400

    update and delete function added

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -7,10 +7,25 @@ from .models import Todo
 def index(request):
     todos = Todo.objects.all()
     return render(request, "base.html", {"todo_list": todos})
+
+
 # post function
 @require_http_methods(["POST"])
 def add(request):
     title = request.POST["title"]
     todo = Todo(title=title)
     todo.save()
+    return redirect("index")
+
+# update function
+def update(request, todo_id):
+    todo = Todo.object.get(id=todo_id)
+    todo.complete = not todo.complete
+    todo.save()
+    return redirect("index")
+
+# delete function 
+def delete(request, todo_id):
+    todo = Todo.object.get(id=todo_id)
+    todo.delete()
     return redirect("index")
